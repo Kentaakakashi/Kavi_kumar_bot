5afb67fdc7590fe11bb679a76df2a34d539e26ff
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-08-30 20:04:02 +0530

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,1 +1,40 @@
+const {
+    REST,
+    Routes
+} = require("discord.js");
 
+const commands = [
+    require("./commands/event"),
+    require("./commands/bucket"),
+    require("./commands/letitout"),
+    require("./commands/stats"),
+    require("./commands/trivia"),
+    require("./commands/countdown"),
+    require("./commands/achievements")
+];
+
+const rest = new REST({ version: "10" })
+    .setToken(process.env.DISCORD_TOKEN);
+
+const commandData =
+    commands.map(command => command.data.toJSON());
+
+(async () => {
+    try {
+        console.log("Registering slash commands...");
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            {
+                body: commandData
+            }
+        );
+
+        console.log("Slash commands registered.");
+    } catch (error) {
+        console.error(error);
+    }
+})();
